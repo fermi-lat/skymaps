67c9ba8cc3412d50f26def0707d295146e1610d0
cvs2git <>
2008-03-02 04:16:03 +0000

    This commit was manufactured by cvs2svn to create tag 'skymaps-00-02-00'.
    Sprout from THB 2008-01-26 22:53:55 UTC burnett 'initial import'
    Cherrypick from master 2008-03-02 04:16:02 UTC burnett 'bypass wcs FITS header processing that fails; add copies of fantastically well done GTI classes for convenience (avoiding unnecssary dependencies); then tagggg':
        cmt/requirements
        doc/release.notes
        skymaps/Gti.h
        skymaps/GtiBase.h
        src/Gti.cxx
        src/GtiBase.cxx
        src/PhotonMap.cxx
        src/SkyImage.cxx
        src/mainpage.h
    Cherrypick from master 2008-02-22 22:40:31 UTC burnett 'cut and paste error :-(':
        ChangeLog
        SConscript
        skymapsLib.py

---
+++ skymaps/Gti.h
@@ -0,0 +1,61 @@
+/**
+ * @file Gti.h
+ * @brief Handle GTIs. Derives from GtiBase, adding a constructor and 
+ * an accept() method.
+ * @author J. Chiang
+ *
+ * Copied by T. Burnett
+ * $Header$
+ */
+
+#ifndef skymaps_Gti_h
+#define skymaps_Gti_h
+
+#include "skymaps/GtiBase.h"
+
+namespace tip {
+   class Table;
+}
+
+namespace skymaps {
+/**
+ * @class Gti
+ * @brief A more useful and complete implementation of GtiBase
+ * @author J. Chiang
+ *
+ */
+
+    class Gti : public skymaps::GtiBase {
+
+public:
+
+   Gti() : skymaps::GtiBase() {}
+
+   Gti(const std::string & filename, const std::string & extension="GTI") 
+      : skymaps::GtiBase(filename, extension) {}
+
+   Gti(const tip::Table & gtiTable);
+
+   Gti(const skymaps::GtiBase & gti) : skymaps::GtiBase(gti) {}
+
+   bool accept(double time) const;
+
+   void writeExtension(const std::string & filename) const;
+
+   /// @brief Given a start and stop time this method recomputes the
+   /// the GTIs.
+   /// @return A new Gti object with the new good-time intervals.
+   /// @param start The start time of the time range cut (MET seconds)
+   /// @param stop The stop time of the time range cut (MET seconds)
+   Gti applyTimeRangeCut(double start, double stop) const;
+
+   /// @return The minimum lower bound of the GTIs (MET seconds)
+   double minValue() const;
+
+   /// @return The maximum upper bound of the GTIs (MET seconds)
+   double maxValue() const;
+
+};
+} // namespace skymaps
+
+#endif // astro_Gti_h
