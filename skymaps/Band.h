53a1b574177116a14d09ff8f435d2bbc815acb9b
burnett <>
2008-05-02 14:44:06 +0000

    remove map inheritance

--- skymaps/Band.h
+++ skymaps/Band.h
@@ -25,8 +25,9 @@ namespace skymaps {
         Implemnt  a map of pixel ids and contents.
         
     */
-    class Band : public astro::SkyFunction, public std::map<int, int> {
+    class Band : public astro::SkyFunction{
     public:
+
         /// default
         Band(): m_nside(-1), m_event_class(0){}
 
@@ -64,11 +65,18 @@ namespace skymaps {
         int query_disk(const astro::SkyDir&dir, double radius, 
             std::vector<std::pair<astro::SkyDir,int> > & v)const;
  
+        /// @brief set a list of pixel ids and counts within the radius about the direction
+        /// @param dir center of cone
+        /// @param radius in radians
+        /// @return the number of photons 
+        int query_disk(const astro::SkyDir&dir, double radius, 
+            std::vector<std::pair<int,int> > & v)const;
+
         /// @brief the solid angle for this pixelization
         double pixelArea()const;
 
-        /// @brief for identity,sorting: assume nside and event class is unique
-        operator int()const{return m_event_class +10* m_nside;}
+        /// @brief for identity,sorting: assume emin and event class is unique
+        operator int()const{return m_event_class +10* static_cast<int>(m_emin+0.5);}
 
         ///@brief count of photons
         int photons()const; 
@@ -78,7 +86,18 @@ namespace skymaps {
         double emax()const{return m_emax;}
         double sigma()const{return m_sigma;}
         double gamma()const{return m_gamma;}
+
+        typedef std::map<int,int> PixelMap;
+        typedef PixelMap::iterator iterator;
+        typedef PixelMap::const_iterator const_iterator;
+        iterator begin(){return m_pixels.begin();}
+        iterator end(){return m_pixels.begin();}
+        const_iterator begin()const{return m_pixels.begin();}
+        const_iterator end()const{return m_pixels.begin();}
+        size_t size()const{return m_pixels.size();}
+
     private:
+        PixelMap m_pixels; 
         int m_nside;
         int m_event_class;
         double m_emin, m_emax;
