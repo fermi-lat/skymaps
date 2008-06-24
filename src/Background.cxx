1e7e950911b109daf9cfa4e532dd0a1e75bbdc64
burnett <>
2008-06-24 17:44:34 +0000

    enhance Background; fix swig

--- src/Background.cxx
+++ src/Background.cxx
@@ -8,44 +8,81 @@ $Header$
 
 #include <stdexcept>
 #include <cmath> 
+#include <iterator>
+#include <sstream>
 
 namespace{  // anonymous namespace for helper classes
 
 }  // anonymous namespace
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 using namespace skymaps;
 
-Background::Background(const skymaps::SkySpectrum& diffuse, 
-                       const skymaps::SkySpectrum& exposure, int n)
-: m_diffuse(diffuse)
-, m_exposure(exposure)
-, m_n(n)
-{
-    if( n<2 || (n&2) !=0 || n>20){
+int Background::s_n(4);
+void Background::set_simpson(int n){
+    s_n=n;
+    if( s_n<2 || (s_n&2) !=0 || s_n>20){
         throw std::invalid_argument("Background--bad Simpsons rule count: n must be even, <20");
     }
+}
+
+Background::Background(const skymaps::SkySpectrum& diffuse, double fixedexposure)
+: m_diffuse(diffuse)
+, m_event_type(0)
+, m_fixedexposure(fixedexposure)
+{
+}
+
+
+Background::Background(const skymaps::SkySpectrum& diffuse, 
+                       const skymaps::SkySpectrum& exposuremap)
+: m_diffuse(diffuse)
+, m_event_type(0)
+{
+    m_exposures.push_back(&exposuremap);
+}
+
+Background::Background(const skymaps::SkySpectrum& diffuse, 
+                       std::vector<const skymaps::SkySpectrum*> exposure_list)
+: m_diffuse(diffuse)
+, m_event_type(0)
+{
+    std::copy(exposure_list.begin(), exposure_list.end(), 
+        std::back_insert_iterator<SpectrumVector>(m_exposures) );
 }
 
 Background::~Background()
 {}
 
+void Background::set_event_type(int n)
+{
+    m_event_type = n;
+    if( n>= m_exposures.size() ){
+        n=0;
+    }
+}
+
+
 double Background::value(const astro::SkyDir& dir, double e)const
 {
-    double val( m_diffuse(dir, e) * m_exposure( dir, e ) );
+    double val( m_diffuse(dir, e) );
+    if( m_exposures.size()==0){
+        val *= m_fixedexposure;
+    }else{
+        val *= m_exposures[m_event_type]->value( dir, e);
+    }
     return val;
 }
 
 ///@brief integral for the energy limits, in the given direction
 double Background::integral(const astro::SkyDir& dir, double a, double b)const
 {
-    double step( log(b/a)/m_n ), // step in log scale
+    double step( log(b/a)/s_n ), // step in log scale
            ratio( exp(step) ), // ratio of energies
            c(2.),              // initial simpsons
            e(a);              //  inital energy
 
     double result( a*value(dir, a) ); // value at low end
-    for( int i = 1; i< m_n; ++i ){
+    for( int i = 1; i< s_n; ++i ){
         e *= ratio; // next energy
         c = 6-c;   // toggle Simpsons coefficient
         result += c * e* value( dir, e); 
@@ -58,5 +95,14 @@ double Background::integral(const astro::SkyDir& dir, double a, double b)const
 
 std::string Background::name()const
 {
-    return "Background: "+ m_diffuse.name()+"+" + m_exposure.name();
+    std::stringstream text;
+    text << "Background: "+ m_diffuse.name();
+    if( m_exposures.empty() ){
+        text <<", Fixed:  "<< m_fixedexposure;
+    }else{
+        for( int i(0); i<m_exposures.size(); ++i){
+            text << ", eventtype=" << i << ": " << m_exposures[i]->name();
+        }
+    }
+    return text.str();
 }
