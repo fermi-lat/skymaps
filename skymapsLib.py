409928cb29748ad1d369798882d8b9935532ae5e
wallacee <>
2008-10-22 07:02:35 +0000

    Modified SConscripts to invoke SWIG.  Added #includes to a few files to make gcc 4.3 happy.

--- skymapsLib.py
+++ skymapsLib.py
@@ -5,7 +5,7 @@
 def generate(env, **kw):
 	if not kw.get('depsOnly',0):
 		env.Tool('addLibrary', library=['skymaps'])
-        depends = 'facilities tip st_facilities healpix'.split()
+        depends = 'facilities tip st_facilities healpix embed_python'.split()
         for pack in depends: env.Tool(pack+'Lib')
 
 	# why these?
