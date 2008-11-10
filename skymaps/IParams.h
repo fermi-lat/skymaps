9f0eada099075621995965021b8a53f8269df74f
mar0 <>
2008-11-10 22:04:40 +0000

    added CALDB fits support through python to IParams

--- skymaps/IParams.h
+++ skymaps/IParams.h
@@ -19,23 +19,53 @@ namespace skymaps {
 
     class IParams {
 public:
-    IParams(std::string& name);
     
+    /** @brief sigma returns PSF parameter sigma
+        @param energy 
+        @param event_class 0=front, 1=back
+        */
     static double sigma(double energy, int event_class);
+
+    /** @brief gamma returns PSF parameter gamma
+        @param energy 
+        @param event_class 0=front, 1=back
+        */
     static double gamma(double energy, int event_class);
 
-    static void set_fp(std::vector<double> params);//{s_fparams=params;}
-    static void set_bp(std::vector<double> params);//{s_bparams=params;}
+    //sets front and back parameterization for sigma
+    // s = (a[0]^2+a[2]*(E/100)^(a[1])+a[3]*(E/100)^(2*a[1]))^0.5
+    static void set_fp(std::vector<double> params);
+    static void set_bp(std::vector<double> params);
 
+    //sets gamma values for front and back set for 4/decade from 100MeV->100GeV
+    //with a bin for <100 MeV and one for >100GeV (15 in all)
+    static void set_fgam(std::vector<double> gams);
+    static void set_bgam(std::vector<double> gams);
 
+    //set energy bins
+    static void set_elist(std::vector<double> elist);
+
+    //run the initialization through the embed python module
+    //the file is psf_defaults.py
+    //the default reads from CALDB files
+    static void init();
+
+    //returns 
     static std::vector<double> params(int event_class);
 
 private:
 
-    static std::vector<double> s_elist;
-    static std::vector<double> s_fparams;
-    static std::vector<double> s_bparams;
+    /** @brief ctor not used
+    */
+    IParams(std::string& name);
 
+    static std::vector<double> s_elist; //energy bin list 4/decade
+    static std::vector<double> s_fparams; //front width parameterization
+    static std::vector<double> s_bparams; //back width parameterization
+    static std::vector<double> s_fgam; //front tail parameterization
+    static std::vector<double> s_bgam; //back tail parameterization
+
+    static bool s_init; //checks to see whether initialization has been performed
 };
 
 }
--- src/IParams.cxx
+++ src/IParams.cxx
@@ -7,6 +7,7 @@ $Header$
 
 #include "skymaps/IParams.h"
 #include "skymaps/PhotonBinner.h"
+#include "embed_python/Module.h"
 #include <cmath>
 #include <algorithm>
 #include <functional>
@@ -16,25 +17,30 @@ using namespace skymaps;
 
 namespace {
 
-    //kluge - all gamma parameterization
-    //double f_p[] = {0.01165278, -1.45237162, 1.67211108, 2.28589082};
-    //double b_p[] = {0.02614202, -1.55092155, 5.18346847, 3.68094493};
+    //default - all gamma parameterization v15r0 
+    double f_p[] = {0.01165278, -1.45237162, 1.67211108, 2.28589082};
+    double b_p[] = {0.02614202, -1.55092155, 5.18346847, 3.68094493};
 
-    double elist[] = {0,100,215,464,1000,2154,4642,10000,21544,46416,100000,1000000};
+    double elist[] = {0   , 100, 178, 316, 562,1000,1778,3162,5623,10000,17783,31623,56234,100000,1000000};
 
-    double fgam[]= {2.25,2.500,2.086,2.132,2.035,1.995,1.860,2.108,2.108,2.141,2.25,2.25};
-    double bgam[]= {2.25,2.243,2.097,2.073,2.192,2.100,2.049,1.983,2.250,2.250,2.25,2.25};
-    //first light -> 07/01/2008-07-27/2008
-    double f_p[] = {0.0, -1.359, 1.638, 0.189};
-    double b_p[] = {0.043, -1.575, 6.476, 2.602};
+    double fgam[]=   {2.65,2.65,2.13,1.97,2.08,2.20,2.43,2.51,2.50, 2.17, 1.94, 1.73, 1.66,  1.62};
+    double bgam[]=   {2.59,2.59,2.09,1.90,1.90,1.64,1.63,1.72,1.76, 1.78, 1.68, 1.71, 1.73,  1.78};
+
 }
 
 
-std::vector<double> IParams::s_elist(elist,elist+12);
+std::vector<double> IParams::s_elist(elist,elist+15);
 std::vector<double> IParams::s_fparams(f_p,f_p+4);
 std::vector<double> IParams::s_bparams(b_p,b_p+4);
+std::vector<double> IParams::s_fgam(fgam,fgam+15);
+std::vector<double> IParams::s_bgam(bgam,bgam+15);
+bool IParams::s_init(false);
+
+void IParams::set_elist(std::vector<double> elist) {s_elist=elist;}
 void IParams::set_fp(std::vector<double> params){s_fparams=params;}
 void IParams::set_bp(std::vector<double> params){s_bparams=params;}
+void IParams::set_fgam(std::vector<double> gams) {s_fgam=gams;}
+void IParams::set_bgam(std::vector<double> gams) {s_bgam=gams;}
 
 IParams::IParams(std::string& name)
 {
@@ -42,6 +48,8 @@ IParams::IParams(std::string& name)
 }
 
 double IParams::sigma(double energy, int event_class){
+    //check to see if initialized, if not do it
+    if(!s_init)  init();
     double deg = 0;
     if(event_class) {
         //from parameterization of PSF function
@@ -53,18 +61,55 @@ double IParams::sigma(double energy, int event_class){
 }
 
 double IParams::gamma(double energy, int event_class) {
+    //check to see if initialized, if not do it
+    if(!s_init)  init();
+
     const std::vector<double>& elisti (s_elist);
 
+    //find nearest energy bin
     std::vector<double>::const_iterator itold=
             std::lower_bound(elisti.begin(), elisti.end(), energy, std::less<double>());
     int level = itold-elisti.begin()-1;
 
-    //return average for now
-    return event_class==0?fgam[level]:bgam[level];
-    //return 2.25;
+    //TODO: implement weighting function
+    return event_class==0?s_fgam[level]:s_bgam[level];
 }
 
 std::vector<double> IParams::params(int event_class) 
 {
     return event_class?s_bparams:s_fparams;
+}
+
+void IParams::init() {
+    s_init=true;
+    int argc=0;
+    char** argv;
+    std::string python_path("../python");
+    try {
+    embed_python::Module setup(python_path , "psf_defaults",  argc, argv);
+    std::vector<double> ps;
+    //get energy list from CALDB file
+    setup.getList("energy",ps);
+    set_elist(ps);
+    ps.clear();
+    //get front sigma paramterization
+    setup.getList("fparams",ps);
+    set_fp(ps);
+    ps.clear();
+    //get back sigma parameterization
+    setup.getList("bparams",ps);
+    set_bp(ps);
+    ps.clear();
+    //now, just get the gamma values
+    setup.getList("fgam",ps);
+    set_fgam(ps);
+    ps.clear();
+    setup.getList("bgam",ps);
+    set_bgam(ps);
+    ps.clear();
+    } catch(const std::exception& e){
+        std::cout << "Caught exception " << typeid(e).name() 
+            << " \"" << e.what() << "\"" << std::endl;
+        std::cout << "Using default PSF values" << std::endl;
+    }
 }
