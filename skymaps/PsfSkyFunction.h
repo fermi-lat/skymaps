5e67fec232afec6f36657d247ee76eb3a9f3d3d9
burnett <>
2009-01-24 22:52:28 +0000

    adding PsfSkyFunction (for real)

---
+++ skymaps/PsfSkyFunction.h
@@ -0,0 +1,51 @@
+/** @file PsfSkyFunction.h
+@brief declare PsfSkyFunction.
+
+$Header$
+*/
+#ifndef skymaps_PsfSkyFunction_h
+#define skymaps_PsfSkyFunction_h
+
+#include "astro/SkyDir.h"
+#include "astro/SkyFunction.h"
+#include "skymaps/PsfFunction.h"
+
+namespace skymaps{
+
+    class PsfFunction;
+
+    /**
+    @class PsfSkyFunction
+    @brief Represents the distribution of the PSF function 
+
+    */
+
+    class PsfSkyFunction : public astro::SkyFunction{
+    public:
+        /** @brief ctor
+        @param roi_dir center of PSF
+        @param gamma value of the power law index (around 2)
+        @param sigma value for the sigma parameter (radians)
+        */
+
+        PsfSkyFunction(const astro::SkyDir & roi_dir, double gamma, double sigma);
+
+        /// @brief implement interface by returning the value
+        double operator () (const astro::SkyDir & r)const;
+
+        /// @brief the average value integrated for the region
+        double average(const astro::SkyDir& dir, double angle, double tolerance)const;
+
+   
+
+
+    private:
+        double level_ave(const astro::SkyDir& dir, double angle, int level) const;
+        astro::SkyDir m_dir;
+        skymaps::PsfFunction m_psf;
+        double m_sigma;
+    };
+
+}
+
+#endif
