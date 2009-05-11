2f16d2724a2bb9ff6bd685f274e52e711e8c212e
mar0 <>
2009-05-11 22:36:56 +0000

    Changed interface to CALDB psf files and added environment variable support

--- skymaps/IParams.h
+++ skymaps/IParams.h
@@ -36,6 +36,8 @@ public:
     // s = (a[0]^2+a[2]*(E/100)^(a[1])+a[3]*(E/100)^(2*a[1]))^0.5
     static void set_fp(std::vector<double> params);
     static void set_bp(std::vector<double> params);
+    static void set_fsig(std::vector<double> sigs);
+    static void set_bsig(std::vector<double> sigs);
 
     //sets gamma values for front and back set for 4/decade from 100MeV->100GeV
     //with a bin for <100 MeV and one for >100GeV (15 in all)
@@ -45,13 +47,17 @@ public:
     //set energy bins
     static void set_elist(std::vector<double> elist);
 
-    //run the initialization through the embed python module
-    //the file is psf_defaults.py
-    //the default reads from CALDB files
-    static void init();
+    //run the initialization of psf parameters
+    //through CALDB files
+    static void init(const std::string& name="P6_v3",
+        const std::string& clevel="diff",const std::string& file="");
+
+    //set the CALDB directory
+    static void set_CALDB(const std::string& dir);
 
     //returns 
     static std::vector<double> params(int event_class);
+    static double scale(double energy, int event_class);
 
 private:
 
@@ -62,8 +68,12 @@ private:
     static std::vector<double> s_elist; //energy bin list 4/decade
     static std::vector<double> s_fparams; //front width parameterization
     static std::vector<double> s_bparams; //back width parameterization
+    static std::vector<double> s_fsig;
+    static std::vector<double> s_bsig;
     static std::vector<double> s_fgam; //front tail parameterization
     static std::vector<double> s_bgam; //back tail parameterization
+
+    static std::string s_CALDB;
 
     static bool s_init; //checks to see whether initialization has been performed
 };
