42fef78f087a82f927df8f5df92bf07c968407cf
kerrm <>
2010-07-06 01:42:48 +0000

    Added a new class CacheExposureMap for caching exposure values at a given energy and conversion type.
    A new method in Exposure to expose the underlying HEALPix binning for the Livetimecube.
    
    Support in Background for using the CacheExposureMap via its SkyFunction interface.
    
    Removed setEnergy from the SWIG extension to Background since it is now implemented in the C++ code.
    
    Added some test code to test_main

--- skymaps/Exposure.h
+++ skymaps/Exposure.h
@@ -11,6 +11,8 @@ $Header$
 #include "skymaps/EffectiveArea.h"
 #include "skymaps/LivetimeCube.h"
 
+//#include "healpix/Healpix.h"
+
 #include <string>
 
 
@@ -66,6 +68,8 @@ public:
     /// @brief set the cosine of the cutoff angle (default = 0.4 <=> 66.4 degrees)
     static void set_cutoff(double cutoff);
 
+    const healpix::Healpix healpix() const {return m_ltcube.data().healpix();}
+
 private:
     const skymaps::LivetimeCube& m_ltcube;
     const skymaps::EffectiveArea& m_aeff;
@@ -78,14 +82,36 @@ private:
 class ExposureMap : public astro::SkyFunction {
 public:
     ExposureMap(const Exposure& exp, double energy): m_exp(exp), m_energy(energy){}
+
     double operator()(const astro::SkyDir& sdir)const{
-        return m_exp(sdir, m_energy);
+        return m_exp.value(sdir,m_energy);
     }
 private:
     const Exposure& m_exp;
     double m_energy;
 };
 
+class CacheExposureMap : public astro::SkyFunction {
+public:
+    CacheExposureMap(const Exposure& exp, double energy): m_exp(exp), m_energy(energy), m_hp(exp.healpix()) {}
+  
+    double operator()(const astro::SkyDir& sdir)const{
+        healpix::Healpix::Pixel pix = m_hp.pixel(sdir);
+        int index(pix.index());
+        std::map<int,double>::const_iterator it = m_cache.find(index);
+        if( it != m_cache.end() ){
+            return (*it).second;
+        }
+        double val (m_exp.value(sdir, m_energy));
+        m_cache[index] = val;
+        return val;
+    }
+private:
+    const Exposure& m_exp;
+    double m_energy;
+    mutable std::map<int,double> m_cache;
+    const healpix::Healpix m_hp;
+};
 
 }
 #endif
