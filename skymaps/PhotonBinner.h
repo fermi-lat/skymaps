574d625f6cf006f13af3bb365ce69da2e089c6d7
burnett <>
2015-06-25 18:00:18 +0000

    implement PSF event type

--- skymaps/PhotonBinner.h
+++ skymaps/PhotonBinner.h
@@ -29,6 +29,7 @@ namespace skymaps {
     public:
          /**@brief ctor (default) 
         */
+        
         PhotonBinner(double bins_per_decade=0);
 
         /** @brief ctor  takes arguments of the left edge bin energy in MeV
@@ -49,13 +50,27 @@ namespace skymaps {
         */
         PhotonBinner(const std::vector<double>&edges, const std::vector<int>&f_nside, const std::vector<int>&b_nside);
 
+        /**@brief ctor user specified bins and nside to be used. This version is for PSF event types
+        * @param edges left edges in MeV (note 1 TeV is always rightmost)
+        * @param psf0_nside Healpix nside parameter for each energy band, psf0
+        * @param psf1_nside Healpix nside parameter for each energy band, psf1
+        * @param psf2_nside Healpix nside parameter for each energy band, psf2
+        * @param psf3_nside Healpix nside parameter for each energy band, psf3
+        */
+        PhotonBinner(const std::vector<double>&edges, 
+            const std::vector<int>&psf0_nside, const std::vector<int>&psf1_nside,
+            const std::vector<int>&psf2_nside, const std::vector<int>&psf3_nside);
+
         virtual ~PhotonBinner(){};
         ///@brief bin a photon by returning an appropriate Band object
         virtual skymaps::Band operator()(const astro::Photon& photon)const;
 		
 		///@brief get the key of the band a photon belongs in
 		int get_band_key(const astro::Photon& photon);
-           
+        
+        //@brief return an index for the event type: 0,1 for front, back, or 2,3,4,5 for PSF0..3 depending value 
+        int event_type_index(int event_type)const;
+        
         static void set_max_nside(unsigned int new_nside);
         static unsigned int  get_max_nside();
 
@@ -70,12 +85,20 @@ namespace skymaps {
         */
         void setupbins();
         double m_bins_per_decade;
-        bool m_user_nside;
+        bool m_user_nside;      // should alwasy be set now
+        bool m_psf_event_types; // switch between old front/back and new psf0..3
         
         std::vector<double> m_bins;           //the energy of each left bin edge
         std::vector<int> m_binlevelmap;       //the mapping between energy bins and healpix levels
         std::vector<int> m_fnside;           // nside parameters for front events
         std::vector<int> m_bnside;           // nside parameters for back events
+
+        // New for PSF event types
+        std::vector<int> m_psf0_nside;           // nside parameters for psf events
+        std::vector<int> m_psf1_nside;           // nside parameters for psf events
+        std::vector<int> m_psf2_nside;           // nside parameters for psf events
+        std::vector<int> m_psf3_nside;           // nside parameters for psf events
+
         static std::vector<double> s_fenergy; //the mapping between energy and healpix levels for front events
         static std::vector<double> s_benergy; //the mapping between energy and healpix levels for back events
         static std::vector<double> s_sigma_level;
@@ -83,6 +106,9 @@ namespace skymaps {
         static unsigned int max_nside;
         static unsigned int min_nside;
         static double m_sigma_scale;
+        
+
+        
     };
 }
 
