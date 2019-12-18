e9f46419e0b5d20180b2bf6ed2e1b5d2c8b30825
Tom Stephens <thomas.stephens@gmail.com>
2019-12-18 10:34:40 -0700

    Fix indention and tabs

--- skymapsLib.py
+++ skymapsLib.py
@@ -3,14 +3,14 @@
 #
 #$Header$
 def generate(env, **kw):
-	if not kw.get('depsOnly',0):
-		env.Tool('addLibrary', library=['skymaps'])
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library=['skymaps'])
         depends = 'facilities tip st_facilities healpix embed_python timeSystem'.split()
         for pack in depends: env.Tool(pack+'Lib')
 
-	# why these?
-	#env.Tool('addLibrary', library=env['clhepLibs'])
-	#env.Tool('addLibrary', library=env['cfitsioLibs'])
+    # why these?
+    #env.Tool('addLibrary', library=env['clhepLibs'])
+    #env.Tool('addLibrary', library=env['cfitsioLibs'])
 
 def exists(env):
-	return 1
+    return 1
