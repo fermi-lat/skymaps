5920ed868c3f4f1f8a555195da0116c909dfba16
Tom Stephens <thomas.stephens@nasa.gov>
2020-04-01 14:57:47 -0400

    Fix indentation error

--- skymapsLib.py
+++ skymapsLib.py
@@ -5,8 +5,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library=['skymaps'])
-        depends = 'facilities tip st_facilities healpix embed_python timeSystem'.split()
-        for pack in depends: env.Tool(pack+'Lib')
+    depends = 'facilities tip st_facilities healpix embed_python timeSystem'.split()
+    for pack in depends: env.Tool(pack+'Lib')
 
     # why these?
     #env.Tool('addLibrary', library=env['clhepLibs'])
